e4816790ee55851218515dddb4b73bb06dcf9716
Drew Nutter <dnut@users.noreply.github.com>
2024-05-29 19:16:04 -0400

    zig 0.12 upgrade

--- sig/install-build-deps.sh
+++ sig/install-build-deps.sh
@@ -5,16 +5,15 @@ set -euxo pipefail
 
 # install zig
 cd $HOME/.local/share/
-wget https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz
-tar xf  zig-linux-x86_64-0.11.0.tar.xz
-rm -rf  zig-linux-x86_64-0.11.0.tar.xz
-wget https://github.com/zigtools/zls/releases/download/0.11.0/zls-x86_64-linux.tar.gz
-tar xf zls-x86_64-linux.tar.gz
-mv bin/zls  zig-linux-x86_64-0.11.0/
+wget https://ziglang.org/download/0.12.0/zig-linux-x86_64-0.12.0.tar.xz
+tar xf  zig-linux-x86_64-0.12.0.tar.xz
+rm -rf  zig-linux-x86_64-0.12.0.tar.xz
+wget https://github.com/zigtools/zls/releases/download/0.12.0/zls-x86_64-linux.tar.xz
+tar xf zls-x86_64-linux.tar.xz
+mv zls  zig-linux-x86_64-0.12.0/
 rm LICENSE
 rm README.md
-rm -r bin/
-rm zls-x86_64-linux.tar.gz
-chmod +x  zig-linux-x86_64-0.11.0/zls
-echo 'PATH=$PATH:$HOME/.local/share/zig-linux-x86_64-0.11.0' >> ~/.profile
+rm zls-x86_64-linux.tar.xz
+chmod +x  zig-linux-x86_64-0.12.0/zls
+echo 'PATH=$PATH:$HOME/.local/share/zig-linux-x86_64-0.12.0' >> ~/.profile
 touch ~/.lldbinit
