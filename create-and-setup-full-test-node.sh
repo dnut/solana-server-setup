3a283b23847bba431a4260cc4fbb706c5568dd0b
Drew Nutter <dnut@users.noreply.github.com>
2024-05-29 19:18:07 -0400

    fix: delay before ssh after ec2 create

--- create-and-setup-full-test-node.sh
+++ create-and-setup-full-test-node.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 ec2/create-instance.sh
+sleep 10
 rsync -r --exclude=.git . agave:/home/ubuntu/setup-server
 ssh agave 'cd setup-server && ./setup-full-test-node.sh'
 ssh agave 'cp setup-server/agave/run-basic-validator.sh run-basic-validator.sh'
