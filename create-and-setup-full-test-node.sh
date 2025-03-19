d3c35569b9f545517a2bdf2a27f27685a858b25f
Drew Nutter <dnut@users.noreply.github.com>
2025-03-18 20:40:43 -0600

    fix: passing SERVER_CONF to remote server (SendEnv does not work due to server config blocking it)

--- create-and-setup-full-test-node.sh
+++ create-and-setup-full-test-node.sh
@@ -2,7 +2,6 @@
 set -euxo pipefail
 
 . "${SERVER_CONF:=server.conf}"
-export SERVER_CONF
 
 # run the script and capture the data that script writes to fd 3 (the ip address)
 exec 4>&1
@@ -11,9 +10,9 @@ exec 4>&-
 sleep 10
 
 # run all the other scripts on the server
-ssh_command="ssh -o StrictHostKeyChecking=no -i $SSH_KEY_PATH -o SendEnv=SERVER_CONF"
+ssh_command="ssh -o StrictHostKeyChecking=no -i $SSH_KEY_PATH"
 rsync -e "$ssh_command" -r --exclude=.git . $AWS_SSH_USER_NAME@$instance_ip:/home/ubuntu/setup-server
-$ssh_command $AWS_SSH_USER_NAME@$instance_ip 'cd setup-server && ./setup-full-test-node.sh'
-$ssh_command $AWS_SSH_USER_NAME@$instance_ip 'cp setup-server/agave/run-basic-validator.sh run-basic-validator.sh'
+$ssh_command $AWS_SSH_USER_NAME@$instance_ip "export SERVER_CONF=$SERVER_CONF && cd setup-server && ./setup-full-test-node.sh"
+$ssh_command $AWS_SSH_USER_NAME@$instance_ip "export SERVER_CONF=$SERVER_CONF && cp setup-server/agave/run-basic-validator.sh run-basic-validator.sh"
 
 echo finished setting up server at $instance_ip
--- setup-full-test-node.sh
+++ setup-full-test-node.sh
@@ -6,10 +6,13 @@ set -euxo pipefail
 
 agave/tune-sys-performance.sh
 agave/setup-filesystems.sh
-agave/install-build-deps.sh && . ~/.profile
-agave/install-init-cli.sh && . ~/.profile
+agave/install-build-deps.sh
+. ~/.profile
+agave/install-init-cli.sh
+. ~/.profile
 agave/clone-and-build.sh
 
-sig/install-build-deps.sh && . ~/.profile
+sig/install-build-deps.sh
+. ~/.profile
 sig/clone-build-init.sh
 sig/install-snapshot-finder.sh
